6160d72528d2b260148d2c8ecc6082a639f05c5d
Ruksanapk <ruksanarukupk.123@gmail.com>
2024-03-17 23:53:54 +0400

    first commit

---
+++ besnus-main/js/script.js
@@ -0,0 +1,50 @@
+let Navbar = document.querySelector('.navbar');
+let Fabars = document.querySelector('.fa-bars');
+
+Fabars.onclick = () =>{
+    Navbar.classList.toggle("active")
+};
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+
+
+var swiper = new Swiper(".home-slid", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+
+var swiper = new Swiper(".team-slider", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      "0": {
+        slidesPerView: 1,
+        autoplay:{
+            delay:700,
+            disableOnInteraction:false,
+        },
+      },
+      "768": {
+        slidesPerView: 2,
+          
+      },
+      "1020": {
+        slidesPerView: 3,
+          
+      },
+    },
+  });
